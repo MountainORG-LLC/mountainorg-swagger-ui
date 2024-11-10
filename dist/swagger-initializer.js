a129b7feebd753d8ff2ea4b11a4d84022be8f059
Sanjaya Marambage <sanjaya.marambage@mountainorg.com>
2024-11-09 20:12:46 -0700

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,9 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    //url: "https://petstore.swagger.io/v2/swagger.json",
+    urls: [{url: "https://category-svc-v6xxfe4qzq-uc.a.run.app/v2/api-docs", name: "Category Service"},
+{url: "https://price-book-svc-69969998642.us-central1.run.app/v3/api-docs", name: "Price Book Service"}],
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
